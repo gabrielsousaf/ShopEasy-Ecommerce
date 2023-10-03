1308559f353aa3cad70eff43a91ac1fabae22d27
gabrielsousaf <gs294860@gmail.com>
2023-10-03 20:28:32 -0300

    Update Cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,23 +2,45 @@ import "./Cart.css";
 import { useCart } from "../../context/CartContext"
 
 const Cart = ({ isOpen, onClose }) => {
-  const { cart, removeFromCart } = useCart();
+  const { cart, removeFromCart, removeAllFromCart, updateCartItemQuantity, calculateTotalPrice } = useCart();
+
+  const handleIncreaseQuantity = (productId) => {
+    updateCartItemQuantity(productId, 1); 
+  };
+
+  const handleDecreaseQuantity = (productId) => {
+    updateCartItemQuantity(productId, -1); 
+  };
+
+  const handleRemoveAll = () => {
+    removeAllFromCart();
+  }
+
+  const totalPrice = calculateTotalPrice();
+
   return (
     <div className={`cart-container ${isOpen ? "open" : ""}`}>
       <div className="cart-content">
-      <ul>
-        {cart.map((product) => (
-          <li key={product.id}>
-            <span>{product.name}</span>
-            <span>R$ {product.price}</span>
-            <span>Quantidade: {product.quantity}</span>
-            <button onClick={() => removeFromCart(product.id)}>
-              Remover
-            </button>
-          </li>
-        ))}
-      </ul>
+        <ul>
+          {cart.map((product) => (
+            <li key={product.id}>
+              <span>{product.name}</span>
+              <span>R$ {product.price}</span>
+              <span>Quantidade: {product.quantity}</span>
+              <button onClick={() => handleDecreaseQuantity(product.id)}>-</button>
+
+              <button onClick={() => handleIncreaseQuantity(product.id)}>+</button>
+              <button onClick={() => removeFromCart(product.id)}>
+                Remover
+              </button>
+            </li>
+          ))}
+        </ul>
+        <button onClick={handleRemoveAll}>Remover Todos</button>
         <button onClick={onClose}>Fechar Carrinho</button>
+        <div className="total-price">
+          <strong>Preço Total: R$ {totalPrice.toFixed(2)}</strong>
+        </div>
       </div>
     </div>
   );
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -12,30 +12,92 @@ export function CartProvider({ children }) {
     return storedCart ? JSON.parse(storedCart) : [];
   });
 
+
+
   const calculateTotalQuantity = () => {
-    return cart.reduce((total, product) => total + product.quantity, 0)
-  }
-
-  const [totalQuantity, setTotalQuantity] = useState(calculateTotalQuantity());
-
+    return cart.reduce((total, product) => total + product.quantity, 0);
+  };
   useEffect(() => {
     setTotalQuantity(calculateTotalQuantity());
-  }, [cart])
+  }, [cart]);
+  const [totalQuantity, setTotalQuantity] = useState(calculateTotalQuantity());
+
+
 
   const addToCart = (product) => {
-    const updatedCart = [...cart, product];
-    setCart(updatedCart);
-    localStorage.setItem("cart", JSON.stringify(updatedCart));
+    const existingProductIndex = cart.findIndex(
+      (item) => item.id === product.id
+    );
+    if (existingProductIndex !== -1) {
+      const updatedCart = [...cart];
+      updatedCart[existingProductIndex].quantity += product.quantity;
+      setCart(updatedCart);
+    } else {
+      const updatedCart = [...cart, product];
+      setCart(updatedCart);
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart));
   };
 
+
+
+  const updateCartItemQuantity = (productId, quantityChange) => {
+    const updatedCart = cart.map((item) => {
+      if (item.id === productId) {
+        if (item.quantity + quantityChange < 1) {
+          return null; 
+        }
+        return {
+          ...item,
+          quantity: item.quantity + quantityChange,
+        };
+      }
+      return item;
+    });
+
+    const filteredCart = updatedCart.filter((item) => item !== null);
+
+    setCart(filteredCart);
+    localStorage.setItem("cart", JSON.stringify(filteredCart));
+  };
+
+
+
   const removeFromCart = (productId) => {
-    // Implemente a lógica para remover produtos do carrinho
     const updatedCart = cart.filter((product) => product.id !== productId);
     setCart(updatedCart);
   };
+  
+
+
+  const calculateTotalPrice = () => {
+    return cart.reduce(
+      (total, product) => total + product.price * product.quantity,
+      0
+    );
+  };
+
+  
+  
+  const removeAllFromCart = () => {
+    setCart([]);
+    localStorage.removeItem("cart");
+  };
+
 
   return (
-    <CartContext.Provider value={{ cart, totalQuantity, addToCart, removeFromCart }}>
+    <CartContext.Provider
+      value={{
+        cart,
+        totalQuantity,
+        updateCartItemQuantity,
+        addToCart,
+        removeFromCart,
+        removeAllFromCart,
+        calculateTotalPrice,
+      }}
+    >
       {children}
     </CartContext.Provider>
   );
